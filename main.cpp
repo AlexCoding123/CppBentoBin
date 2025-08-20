cef25840fa2736443ff08935ea4c2e9c8a34cd12
Alex Davila Alonso <alex.davilaalonso@dawsoncollege.qc.ca>
2025-08-20 19:25:15 -0400

    Start reading from bento binary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 
-int main(){
+int main(int argc, char* argv[]){
+    std::fstream fout;
+    std::string filename = argv[1];
+
+    fout.open("bento-data.bin", std::ios::out | std::ios::binary);
+
+    if(fout){
+        fout.write();
+    }
+
     return 0;
 }
